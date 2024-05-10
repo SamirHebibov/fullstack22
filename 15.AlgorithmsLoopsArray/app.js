2ff7b9cafb97b9af9da18bbda02386366002db34
SamirHebibov <hebibovsamir26@gmail.com>
2024-05-10 23:55:25 +0400

    loops arrays task part 1

---
+++ 15.AlgorithmsLoopsArray/app.js
@@ -0,0 +1,71 @@
+
+//task1
+// let n = Number(prompt("ededi daxil edin:"));
+// let count = 0; 
+// while(n>=1){
+//       n = n / 10;
+//       count++;
+// }
+// console.log(count);
+
+
+
+// task2
+// let cumle = prompt("setri daxil edin:");
+// let soz = cumle.split(",").length; 
+// console.log(soz);
+
+
+// task3
+// let num1 = Number(prompt("ededi daxil edin:")); 
+// let num2 = Number(prompt("quvvetin derecesini daxil edin:")); 
+// console.log(Math.pow(num1, num2));
+
+
+// task4
+// let arr = [1,2,3,4,5,6,7,8]; 
+// let sum = 0; 
+// for (let i = 0; i <= arr.length; i++) {
+//       sum = sum+i; 
+// }
+// console.log(sum/arr.length);
+
+
+// task5 
+// let arr = [1,2,3,4,5,6,7,8]; 
+// min=arr[0]; 
+// max= arr[0]; 
+// for (let i = 0; i <= arr.length; i++) {
+//       if(arr[i]<min){
+//             min=arr[i];
+//       }
+//       if(arr[i]>max){
+//             max=arr[i];
+//       }
+// }
+// console.log((min+max)/2);
+
+
+// task6
+// let arr = [2,5,7,8,4,21,13,3,11];
+// count=0; 
+// for (let i = 0; i <= arr.length; i++) {
+//       if(arr[i]==2){
+//             count++;
+//       }
+//       else if(arr[i]%6==1 || arr[i]%6==5){
+//             count++;
+//       }
+// }
+// console.log(count);
+
+
+// task7
+// let n = Number(prompt("ededi daxil edin:")); 
+// while(n >=1){
+//       console.log(Math.round(n%10));
+//       n/=10; 
+// }
+
+
+// task8
